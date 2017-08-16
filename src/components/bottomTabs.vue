fe2cc8355191ae835865a8ee455061cce1f6b957
Aleksandr Tsaytler <dev@goodea.ru>
2017-08-17 02:00:40 +0300

    s1 temperarytemporarytemporary component

---
+++ src/components/bottomTabs.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+
+  <!-- <f7-toolbar bottom scrollable>
+    <f7-link>Left Link</f7-link>
+    <f7-link>Right Link</f7-link>
+</f7-toolbar> -->
+<f7-toolbar tabbar bottom labels>
+    <f7-link tab-link="#tab1" class="bottom-tabs">Tab 1</f7-link>
+  <f7-link tab-link="#tab2" class="bottom-tabs">Tab 2</f7-link>
+  <f7-link tab-link="#tab3" class="bottom-tabs">Tab 3</f7-link>
+
+</f7-toolbar>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'BottonTabs',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="css">
+.bottom-tabs {
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/panel.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+
+
+    <f7-panel left reveal layout="dark">
+			<f7-view id="left-panel-view" navbar-through :dynamic-navbar="true">
+				<f7-navbar title="Left Panel"></f7-navbar>
+				<f7-pages>
+					<f7-page>
+						
+					</f7-page>
+				</f7-pages>
+			</f7-view>
+		</f7-panel>
+
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'panel',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .panel {
+
+  }
+</style>
