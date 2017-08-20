4555d832ec7ce663e0d9384da54c0c3bd5aedc65
Aleksandr Tsaytler <dev@goodea.ru>
2017-08-20 09:51:29 +0300

    save b64 to jpeg

--- src/components/photograb.vue
+++ src/components/photograb.vue
@@ -17,11 +17,12 @@
 </template>
 
 <script lang="js">
+ import mfsService from '../services/mfs.service.js'
 
   function setOptions(srcType) {
      var options = { 
       quality: 50,
-      destinationType: Camera.DestinationType.FILE_URI,
+      destinationType: Camera.DestinationType.DATA_URL,
       sourceType: srcType,
       encodingType: Camera.EncodingType.JPEG,
       mediaType: Camera.MediaType.PICTURE,
@@ -62,8 +63,6 @@
         let cam = document.querySelectorAll(".cordova-camera-capture")[0];
         cam.style.position = "fixed";
         cam.style.top = "100px";
-
-        
       },
       openFilePicker: function(selection) {
 
@@ -74,8 +73,8 @@
           navigator.camera.getPicture(function cameraSuccess(imageUri) {
 
               var image = document.getElementById('myImage');
-              // image.src = "data:image/jpeg;base64," + imageUri;
-              image.src = imageUri;
+              image.src = "data:image/jpeg;base64," + imageUri;
+              // image.src = imageUri;
 
           }, function cameraError(error) {
               alert("Unable to obtain picture: " + error, "app");
@@ -84,15 +83,24 @@
       },
       onSuccess: function(imageURI) {
 	      var image = document.getElementById('myImage');
-	      // image.src = "data:image/jpeg;base64," + imageURI;
-	      image.src = imageURI;
+	      image.src = "data:image/jpeg;base64," + imageURI;
+	      // image.src = imageURI;
         
         photoObj.fileUri = imageURI;
         photoObj.fileImg64 = image.src;
         // console.log(blob);
         try {
-          alert('createNewFileEntry begin');
-          createNewFileEntry(imageURI);
+          // alert('createNewFileEntry begin');
+          let now = Date.now();
+          // To define the type of the Blob
+          var contentType = "image/jpeg";
+          // if cordova.file is not available use instead :
+          var folderpath = cordova.file.externalDataDirectory + "/images/"; 
+          //cordova.file.externalDataDirectory
+          var filename = "gdapp_" + now.toString() + ".jpeg";
+          mfsService.post(folderpath, filename, imageURI, contentType);
+
+          // createNewFileEntry(imageURI);
         } catch (error) {
           alert(error);
         }
@@ -107,114 +115,77 @@
 
 
 
-function createNewFileEntry(imgUri) {
-    window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function success(dirEntry) {
+// function createNewFileEntry(imgUri) {
+//     window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function success(dirEntry) {
 
-        // JPEG file
-        let now = Date.now();
-        alert(now);
-        alert(cordova.file.dataDirectory);
-        alert(cordova.file.cashDirectory);
-        alert(cordova.file.externalDataDirectory);
-        let fileName = "gdapp_" + now.toString() + ".jpeg";
-        dirEntry.getFile(fileName, { create: true, exclusive: false }, function (fileEntry) {
+//         // JPEG file
+//         let now = Date.now();
+//         alert(now);
+//         alert(cordova.file.dataDirectory);
+//         alert(cordova.file.cashDirectory);
+//         alert(cordova.file.externalDataDirectory);
+//         let fileName = "gdapp_" + now.toString() + ".jpeg";
+//         dirEntry.getFile(fileName, { create: true, exclusive: false }, function (fileEntry) {
 
-            alert(fileName);
-            // Do something with it, like write to it, upload it, etc.
-            writeFile(fileEntry, imgUri);
-            alert("#1 got file: " + fileEntry.fullPath);
-            // alert("#1 got file: " + fileEntry.fullPath);
-            // for (var key in fileEntry) {
-            //   alert("#1_1 got file: " + fileEntry[key]);  
-            // }
+//             alert(fileName);
+//             // Do something with it, like write to it, upload it, etc.
+//             writeFile(fileEntry, imgUri);
+//             alert("#1 got file: " + fileEntry.fullPath);
+//             // alert("#1 got file: " + fileEntry.fullPath);
+//             // for (var key in fileEntry) {
+//             //   alert("#1_1 got file: " + fileEntry[key]);  
+//             // }
             
-            // displayFileData(fileEntry.fullPath, "File copied to");
+//             // displayFileData(fileEntry.fullPath, "File copied to");
 
-        }, (err) => alert(err));
+//         }, (err) => alert(err));
 
-    }, (err) => alert(err));
-}
+//     }, (err) => alert(err));
+// }
 
-function getFileEntry(imgUri) {
-    window.resolveLocalFileSystemURL(imgUri, function success(fileEntry) {
+// function writeFile(fileEntry, dataObj) {
+//     // Create a FileWriter object for our FileEntry (log.txt).
+//     fileEntry.createWriter(function (fileWriter) {
 
-        // Do something with the FileEntry object, like write to it, upload it, etc.
-        writeFile(fileEntry, imgUri);
-        alert("#2 got file: " + fileEntry.fullPath);
-        alert("#2_1 got file: " + fileEntry);
-        // displayFileData(fileEntry.nativeURL, "Native URL");
+//         fileWriter.onwriteend = function() {
+//             alert("Successful file write...");
+//             if (dataObj.type == "image/png") {
+//                 readBinaryFile(fileEntry);
+//             }
+//             else {
+//                 readFile(fileEntry);
+//             }
+//         };
 
-    }, function () {
-      // If don't get the FileEntry (which may happen when testing
-      // on some emulators), copy to a new FileEntry.
-        createNewFileEntry(imgUri);
-    });
-}
-function writeFile(fileEntry, dataObj) {
-    // Create a FileWriter object for our FileEntry (log.txt).
-    fileEntry.createWriter(function (fileWriter) {
+//         fileWriter.onerror = function (e) {
+//             alert("Failed file write: " + e.toString());
+//         };
 
-        fileWriter.onwriteend = function() {
-            alert("Successful file write...");
-            if (dataObj.type == "image/png") {
-                readBinaryFile(fileEntry);
-            }
-            else {
-                readFile(fileEntry);
-            }
-        };
+//         // If data object is not passed in,
+//         // create a new Blob instead.
+//         if (!dataObj) {
+//             // dataObj = new Blob(['some file data'], { type: 'text/plain' });
+//         }
 
-        fileWriter.onerror = function (e) {
-            alert("Failed file write: " + e.toString());
-        };
-
-        // If data object is not passed in,
-        // create a new Blob instead.
-        if (!dataObj) {
-            // dataObj = new Blob(['some file data'], { type: 'text/plain' });
-        }
-
-        fileWriter.write(dataObj);
-    });
-}
+//         fileWriter.write(dataObj);
+//     });
+// }
 
 function onErrorLoadFs(err) {
   console.log(err);
 }
 
-function readBinaryFile(fileEntry) {
-
-    fileEntry.file(function (file) {
-        var reader = new FileReader();
-
-        reader.onloadend = function() {
-
-            console.log("Successful file write: " + this.result);
-            // displayFileData(fileEntry.fullPath + ": " + this.result);
-
-            var blob = new Blob([new Uint8Array(this.result)], { type: "image/png" });
-            // displayImage(blob);
-        };
-
-        reader.readAsArrayBuffer(file);
-
-    }, (err) => {alert(err);});
-}
 
 function getSampleFile(params) {
-
-
- 
-  // var blob = new Blob([this.response], { type: 'image/png' });
-  // saveFile(dirEntry, blob, "downloadedImage.png");
   
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang="css">
 .photograb {
   border: 4px solid red;
 }
+
 .cordova-camera-capture {
   position: fixed !important;
   top: 100px;
